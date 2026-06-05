45b100dc8a80345b49e543c09927800d9b756e8d
zonor121 <kopinator9000@gmail.com>
2026-06-05 06:14:31 +0300

    первое задание

---
+++ 01 Модуль 1. База- что и зачем тестируем/Практика/qa-unittest-homework/src/qautils/slugify.py
@@ -0,0 +1,9 @@
+import re
+
+
+def slugify(text: str) -> str:
+    slug = text.lower()
+    slug = re.sub(r"[\s_]+", "-", slug)
+    slug = re.sub(r"[^a-z0-9-]", "", slug)
+    slug = re.sub(r"-+", "-", slug)
+    return slug.strip("-")
